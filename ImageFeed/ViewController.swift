e3e3048a823286d763246c9ccd2d69ce9699a2d1
udachi tomo <fragnet1ck@gmail.com>
2023-02-18 19:54:23 +0300

    no message

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by User on 18.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
